e181accff8195a1642f01121d61927fe17d39890
den feofanov <omail4me@gmail.com>
2018-06-30 11:45:02 +0300

    [feat] add index.html to launch bundled script in a browser

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,10 @@
 import leftPad from "./leftpad";
 
 const serNos = [6934, 23111, 23114, 1001, 211161];
-const strSNos = serNos.map(sn => leftPad(sn, 8, "0"));
-console.log(strSNos);
+const partEl = document.getElementById("part-list");
+const strList = serNos.reduce(
+  (acc, element) => (acc += `<li>${leftPad(element, 8, "0")}</li>`),
+  ""
+);
+
+partEl.innerHTML = strList;
